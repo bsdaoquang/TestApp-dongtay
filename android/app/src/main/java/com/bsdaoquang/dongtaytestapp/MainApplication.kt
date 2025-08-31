a02543b59a0381ff6bfbb5993a3a4d3ced004f74
bsdaoquang <bsdaoquang@gmail.com>
2025-08-31 14:31:26 +0700

    connect to firebase firestore and show list project

--- android/app/src/main/java/com/bsdaoquang/dongtaytestapp/MainApplication.kt
+++ android/app/src/main/java/com/bsdaoquang/dongtaytestapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.testapp
+package com.bsdaoquang.dongtaytestapp
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/DongTay/AppDelegate.swift
+++ ios/DongTay/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "TestApp",
+      withModuleName: "Dong Tay",
       in: window,
       launchOptions: launchOptions
     )
